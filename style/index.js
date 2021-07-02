d105af933eec9a92c0f06a653021a2688efcb5a1
olukayode ifeoluwa <ifeoluwajoshua2015@gmail.com>
2021-07-02 17:23:16 +0100

    task1 node stack+ decagon

--- style/index.js
+++ style/index.js
@@ -1,5 +1,35 @@
-function main(){
+for (let pointer = 1; pointer <= 10; pointer++) {
+  main(pointer);
+  let clickPointer = document.getElementById(`charname${pointer}`);
+  clickPointer.addEventListener("click", () => displayInfo(pointer));
 
+  function displayInfo(pointer) {
+    let hiddenInfo = document.getElementById(`text${pointer}`);
+    if (hiddenInfo.style.display === "none") {
+      hiddenInfo.style.display = "block";
+    } else {
+      hiddenInfo.style.display = "none";
+    }
+  }
 }
 
-module.exports = {main}
+function main(pointer) {
+  fetch(`https://swapi.dev/api/people/${pointer}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById(`charname${pointer}`).innerHTML = data.name;
+      document.getElementById(`text${pointer}`).innerHTML =
+        "Name: " +
+        data.name +
+        "<br/>" +
+        "Height: " +
+        data.height +
+        "<br/>" +
+        "Gender: " +
+        data.gender;
+    });
+}
+
+//main();
+
+//module.exports = { main };
