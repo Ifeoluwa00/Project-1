50651a318c6e8c40162e33fd8774bdaef92f2390
olukayode ifeoluwa <ifeoluwajoshua2015@gmail.com>
2021-07-03 21:37:53 +0100

    final changes made and ready to be submitted

--- style/index.js
+++ style/index.js
@@ -1,35 +1,49 @@
-for (let pointer = 1; pointer <= 10; pointer++) {
-  main(pointer);
-  let clickPointer = document.getElementById(`charname${pointer}`);
-  clickPointer.addEventListener("click", () => displayInfo(pointer));
+function main() {
+  const movieslist = document.querySelector(".movieslist");
+  const images = [
+    "luke skywalker.jpeg",
+    "C-3PO.jpeg",
+    "R2-D2.jpeg",
+    "Darth Vader.jpeg",
+    "Leia Organa.png",
+    "Owen Lars.jpeg",
+    "Beru Whitesun lars.jpeg",
+    "R5-D4.jpeg",
+    "Biggs Darklighter.jpeg",
+    "Obi-Wan Kenobi.jpeg",
+  ];
 
-  function displayInfo(pointer) {
-    let hiddenInfo = document.getElementById(`text${pointer}`);
-    if (hiddenInfo.style.display === "none") {
-      hiddenInfo.style.display = "block";
-    } else {
-      hiddenInfo.style.display = "none";
-    }
-  }
+  document.addEventListener("DOMContentLoaded", () => {
+    const url = "https://swapi.dev/api/people";
+    output = "";
+    return fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        data.results.forEach((item, index) => {
+          output += `
+          <div class="movieitems">
+          <img src="./star_wars_images/${images[index]}" alt="images">
+          <div class="movietext">
+              <h3>NAME: ${item.name}</h3>
+          </div>
+          <div class="card__details hidden">
+              <h3>HEIGHT: ${item.height}</h3>
+              <h3>GENDER: ${item.gender}</h3>
+          </div>
+      </div>
+          `;
+        });
+        movieslist.innerHTML = output;
+        const movieText = document.querySelectorAll(".movietext");
+        const card__details = document.querySelectorAll(".card__details");
+        movieText.forEach((item, index) => {
+          item.addEventListener("click", (event) =>
+            card__details[index].classList.toggle("hidden")
+          );
+        });
+      })
+      .catch((error) => console.log(error));
+  });
 }
 
-function main(pointer) {
-  fetch(`https://swapi.dev/api/people/${pointer}`)
-    .then((response) => response.json())
-    .then((data) => {
-      document.getElementById(`charname${pointer}`).innerHTML = data.name;
-      document.getElementById(`text${pointer}`).innerHTML =
-        "Name: " +
-        data.name +
-        "<br/>" +
-        "Height: " +
-        data.height +
-        "<br/>" +
-        "Gender: " +
-        data.gender;
-    });
-}
-
-//main();
-
-//module.exports = { main };
+main();
